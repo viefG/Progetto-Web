2a9ec3a10318113f38eccebb99c77eec223cfe99
Luciano <150362199+viefG@users.noreply.github.com>
2024-01-08 12:06:23 +0100

    Add files via upload

--- script/script1.js
+++ script/script1.js
@@ -4,3 +4,8 @@ function dest() {
     const destinazione = 'index2.html'; // Specifica il nome del file di destinazione
     window.location.href = destinazione + '?targa=' + encodeURIComponent(targa1);
 }
+
+function back(){
+    const destinazione2 = 'index.html';
+    window.location.href = destinazione2;
+}
--- script/script2.js
+++ script/script2.js
@@ -3,8 +3,10 @@ function rivela_targa() {
     const targaInserita = params.get('targa');
     console.log(targaInserita);
     var targa_da_inserire = document.getElementById("targa2");
-    targa_da_inserire.textContent = "Targa " + targaInserita;
+    targa_da_inserire.textContent = "Targa: " + targaInserita;
     var kilometri2 = document.getElementById('kilometri');
+    let controllo=0;
+    var tempoRimanente = 5; // secondi
 
     let link = 'https://www.ilportaledellautomobilista.it/interrogazionistoricorevisioni/api/v1/storicorevisioni/A/';
 
@@ -13,7 +15,7 @@ function rivela_targa() {
         {
             mode: "cors",
             method: "POST",
-            body: '{ "id": "48967587645270284387", "text": "MwJuag" }',
+            body: '{ "id": "48967587645265250212", "text": "nmApup5i" }',
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
@@ -35,6 +37,19 @@ function rivela_targa() {
             )
                 .then((response) => response.json())
                 .then((data) => {
+                    console.log(data);
+                    if (data.codice=='499'){
+                        kilometri2.textContent='Errore, auto non trovata';
+                        var intervallo = setInterval(function() {
+                            document.getElementById("messaggio").innerText = "Verrai riportato nella schermata principale tra " + tempoRimanente + " secondi";
+                            tempoRimanente--;
+
+                            if (tempoRimanente < 0) {
+                                clearInterval(intervallo);
+                                window.location.href = 'index.html';
+                            }
+                        }, 1000); // Aggiorna ogni secondo
+                    }
                     kilometri1  = data.informations[0].numKmiPcsRvs;
                     console.log(data);
                     kilometri2.textContent='La macchina ha percorso km: ' +kilometri1;
